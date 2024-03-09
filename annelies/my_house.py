fcc405b78f5cea8f13bf26adfab0d615c77f3d14
Annelies <annelies2000@outlook.com>
2024-03-09 17:32:36 +0100

    clean up code

--- annelies/my_house.py
+++ annelies/my_house.py
@@ -639,6 +639,7 @@ def makeHouse(posX, posY, posZ, width, depth, rotated=False):
                         geo.placeCuboid(ED, (stairsStartX - stairsWidth, housePosition[1] + floorThickness + i, housePosition[2]+wallThickness), (stairsStartX - stairsWidth, housePosition[1] + floorThickness + i, housePosition[2]+wallThickness+1), Block('purpur_stairs', {"facing": "west"}) )
 
             # room for bathroom right from the stairs
+            
             roomOnTheRight = (housePosition[0] + houseWidth) - stairsStartX 
             if(roomOnTheRight >= 6):
                   makeBathRoom(randint(stairsStartX, (stairsStartX + roomOnTheRight) - 6), housePosition[1] + roomHeight, housePosition[2] + wallThickness, False)
@@ -731,20 +732,6 @@ def makeHouse(posX, posY, posZ, width, depth, rotated=False):
                   geo.placeCuboid(ED, (lightsStartX + x, lightsY, lightsZ), (lightsStartX + x, lightsY, lightsZ), Block('beacon'))
 
 
-      # make bed + nightstand 
-      # geo.placeCuboid(ED, (housePosition[0] + houseWidth - (wallThickness * 2), housePosition[1] + floorThickness + roomHeight, housePosition[2] + houseDepth - 2), (housePosition[0] + houseWidth - (wallThickness * 2), housePosition[1] + floorThickness + roomHeight, housePosition[2] + houseDepth - 2), Block('pink_bed', {"facing": "east"}))
-      # geo.placeCuboid(ED, (housePosition[0] + houseWidth - (wallThickness * 2), housePosition[1] + floorThickness + roomHeight, housePosition[2] + houseDepth - 3), (housePosition[0] + houseWidth - (wallThickness * 2), housePosition[1] + floorThickness + roomHeight, housePosition[2] + houseDepth - 3), Block('pink_bed', {"facing": "east"}))
-      # geo.placeCuboid(ED, (housePosition[0] + houseWidth - wallThickness, housePosition[1] + floorThickness + roomHeight, housePosition[2] + houseDepth - wallThickness), (housePosition[0] + houseWidth - wallThickness, housePosition[1] + floorThickness + roomHeight, housePosition[2] + houseDepth - wallThickness), Block('enchanting_table'))
-      
-      # make rug
-                  
-      # place flowers in pots
-                  
-      # place amethyst blocks
-                  
-      # place paintings
-
-
       print("Make house!")
 
       return
@@ -784,23 +771,6 @@ def makeSmallBathRoom(posX, posY, posZ):
       geo.placeCuboid(ED, (posX + 1, posY+1, posZ), (posX + 1, posY+1, posZ+1), Block("birch_trapdoor", {"facing": "west", "open": "true", "half": "bottom"}))
       geo.placeCuboid(ED, (posX + 2, posY+1, posZ), (posX + 2, posY+1, posZ), Block("quartz_stairs", {"facing": "east", "waterlogged": "true"}))
       geo.placeCuboid(ED, (posX + 2, posY+1, posZ+1), (posX + 2, posY+1, posZ+1), Block("quartz_stairs", {"facing": "south", "waterlogged": "true"}))
-
-
-      # if(left):
-
-      #       geo.placeCuboid(ED, (posX, posY, posZ), (posX, posY + 2, posZ+1), Block("diamond_block"))
-      #       geo.placeCuboid(ED, (posX + 5, posY, posZ), (posX + 5, posY + 2, posZ+1), Block("diamond_block"))
-      # else:
-      #       geo.placeCuboid(ED, (posX, posY, posZ), (posX, posY + 2, posZ+1), Block("diamond_block"))
-      #       geo.placeCuboid(ED, (posX, posY, posZ), (posX, posY + 2, posZ+1), Block("diamond_block"))
-
-      #       geo.placeCuboid(ED, (posX + 4, posY, posZ+1), (posX + 4, posY + 2, posZ+1), Block("quartz_stairs"))
-      #       geo.placeCuboid(ED, (posX + 5, posY, posZ+1), (posX + 5, posY + 2, posZ+1), Block("quartz_stairs"))
-      #       geo.placeCuboid(ED, (posX + 4, posY, posZ), (posX + 4, posY + 2, posZ+1), Block("quartz_stairs"))
-      #       geo.placeCuboid(ED, (posX + 5, posY, posZ), (posX + 5, posY + 2, posZ+1), Block("quartz_stairs"))
-      #       geo.placeCuboid(ED, (posX + 4, posY, posZ-1), (posX + 4, posY + 2, posZ-1), Block("smooth_quartz"))
-      #       geo.placeCuboid(ED, (posX + 5, posY, posZ-1), (posX + 5, posY + 2, posZ-1), Block("smooth_quartz"))
-
 
 def get_surface_size(a):
       return a['surfaceSize']
@@ -872,7 +842,6 @@ def mapSameValueAreasInMatrix(g):
             width = 0
             depth = 0
       
-
       return {
             'height': g['height'],
             'startX': startX,
@@ -894,25 +863,11 @@ def calculateArea():
 
       for index in enumerate(heights):
             sameValueGroupsPerRow.append([])
-      # print("0,0:", heights[0][0])
-      # print("8,8:", heights[8][8])
-      # print("0,8:", heights[0][8])
-      # # print("99,99:", heights[99][99])
-      # print("100,100:", heights[100][100])
-      # # print("100,0:", heights[100][0])
-      # # print("99,0:", heights[99][0])
-      # # print("-2,-2:", heights[-2][-2])
-      # # print("-1,-1:", heights[-1][-1])
-
-      # geo.placeCuboid(ED, (STARTX, STARTY, STARTZ), (LASTX, -54, STARTZ), Block('red_concrete'))
 
       for ri, row in enumerate(heights):
-            print("ri:", ri)
-
             sameValueGroupCurrentIndex = 0
 
             for ci, col in enumerate(row):
-                  print("ci:", ci)
                   if(ci > 0):
                         # check if block it not water or lava.
                         block = WORLDSLICE.getBlock((ri, col-1, ci))
@@ -929,9 +884,6 @@ def calculateArea():
                         sameValueGroupsPerRow[ri].append({"height":col, "coordinates": []})
                         sameValueGroupsPerRow[ri][sameValueGroupCurrentIndex]['coordinates'].append(ci)
                   
-                  # print('sameValueGroupsPerRow[ri]', sameValueGroupsPerRow[ri])
-                  # print('')
-            print("ri:", ri)
       # Filter out the groups that have no coordinates.
       for index, row in enumerate(sameValueGroupsPerRow):
             sameValueGroupsPerRow[index] = list(filter(has_coordinates, row))
@@ -1047,297 +999,10 @@ def calculateArea():
             return 
 
 
-      # plt.imshow(bigAreas)
-      # plt.show()
-      # for s in enumerate(sameValueAreasInMatrix):
-            # print(s)
-
-      # print(sameValueAreasInMatrix)
-
-# if a situation occurs where there are at least 10 (minHouseWidth) samevaluegroup coordinates starting with 3 ([3, 3] [3,4] [3,5]..) and  7 rows are the same but with different first coordinate ([4,3] [4,4] [4,5] and [5,3] [5,4] [5,5] and [6,3] [6,4] [6,5] etc. then build a house there. otherwise chop trees around biggest spot.)
-# for x in range(0, 10 + 1):
-#     # The northern wall
-#     y = heights[(x - 0, 0)]
-#     geo.placeCuboid(ED, (x, y - 2, STARTZ), (x, y, STARTZ), Block("granite"))
-#     geo.placeCuboid(ED, (x, y + 1, STARTZ), (x, y + 4, STARTZ), Block("granite_wall"))
-#     # The southern wall
-#     y = heights[(x - 0, LASTZ - STARTZ)]
-#     geo.placeCuboid(ED, (x, y - 2, LASTZ), (x, y, LASTZ), Block("red_sandstone"))
-#     geo.placeCuboid(ED, (x, y + 1, LASTZ), (x, y + 4, LASTZ), Block("red_sandstone_wall"))
-
-# print("Building north-south walls...")
-# for x in range(STARTX, LASTX + 1):
-#     for y in range(STARTY, LASTY + 1):
-#         geo.placeCuboid(ED,)
-
-# fill the entire buildarea
-# geo.placeCuboid(ED, (STARTX, STARTY, STARTZ), (LASTX, LASTY, LASTZ), Block("sandstone"));
     
-# fill the entire buildarea with air
-# geo.placeCuboid(ED, (STARTX, STARTY, STARTZ), (LASTX, LASTY, LASTZ), Block("air"));
-    
-
-
-
-# search good spot that fits a house
-
-# houseWidth = 10
-# houseHeight = 5
-# houseDepth = 7
-
-# wallThickness = 1
-# floorThickness = 1
-
-# housePosition = (STARTX, STARTY, STARTZ)
-
-# # # place house
-# # geo.placeCuboidHollow(ED, housePosition, (housePosition[0] + houseWidth, housePosition[1] + houseHeight, housePosition[2] + houseDepth), Block("pink_concrete"));
-
-# # # cut out wall
-# # geo.placeCuboid(ED, (housePosition[0] + wallThickness, housePosition[1] + floorThickness, housePosition[2] + houseDepth - wallThickness), (housePosition[0] + houseWidth - wallThickness, housePosition[1] + houseHeight - floorThickness, housePosition[2] + houseDepth), Block("air"));
-
-# # # place floor
-# # geo.placeRect(ED, Rect((housePosition[0]+wallThickness,housePosition[2]+wallThickness),(houseWidth - wallThickness, houseDepth)), housePosition[1], Block("sandstone"))
-
-# # # place window left
-# # geo.placeCuboid(ED, (housePosition[0] + 3, housePosition[1] + 2, housePosition[2]), (housePosition[0] + 3, housePosition[1] + 5, housePosition[2]), Block("glass_pane"))
-
-# # # place window right
-# # geo.placeCuboid(ED, (housePosition[0] + houseWidth - 3, housePosition[1] + 2, housePosition[2]), (housePosition[0] + houseWidth - 3, housePosition[1] + 5, housePosition[2]), Block("glass_pane"))
-
-# # # place big window center
-# # geo.placeCuboid(ED, (housePosition[0] + (houseWidth / 2) - 2, housePosition[1] + 2, housePosition[2]), (housePosition[0] + (houseWidth/2) + 2, housePosition[1] + 5, housePosition[2]), Block("glass_pane"))
-
-
-
-
-
-# fig, ax = plt.subplots()
-# min_val, max_val = STARTX, STARTZ
-
-# intersection_matrix = np.random.randint(0, 10, size=(max_val, max_val))
-
-# ax.matshow(intersection_matrix, cmap=plt.cm.Blues)
-
-# for i in range(15):
-#       for j in range(15):
-#             c = intersection_matrix[j,i]
-#             ax.text(i, j, str(c), va='center', ha='center')
-
-# plt.matshow(intersection_matrix, cmap=plt.cm.Blues)
-
-# ax.set_xlim(min_val, max_val)
-# ax.set_ylim(min_val, max_val)
-# ax.set_xticks(np.arange(max_val))
-# ax.set_yticks(np.arange(max_val))
-# ax.grid()
-
-
-
-
-
-
-
 
 
 
 
 
 calculateArea()
-
-
-
-
-
-
-
-# for x in range(0, 30 + 1):
-#     # The northern wall
-#     y = heights[(x - 0, 0)]
-#     print(heights[(x - 0, 0)]);
-#     geo.placeCuboid(ED, (x, y - 2, STARTZ), (x, y, STARTZ), Block("granite"))
-#     geo.placeCuboid(ED, (x, y + 1, STARTZ), (x, y + 4, STARTZ), Block("granite_wall"))
-# #     # The southern wall
-# #     y = heights[(x - 0, LASTZ - STARTZ)]
-# #     geo.placeCuboid(ED, (x, y - 2, LASTZ), (x, y, LASTZ), Block("red_sandstone"))
-# #     geo.placeCuboid(ED, (x, y + 1, LASTZ), (x, y + 4, LASTZ), Block("red_sandstone_wall"))
-
-# print("Building north-south walls...")
-
-
-# def buildPerimeter():
-#     """Build a wall along the build area border.
-
-#     In this function we're building a simple wall around the build area
-#         pillar-by-pillar, which means we can adjust to the terrain height
-#     """
-#     # HEIGHTMAP
-#     # Heightmaps are an easy way to get the uppermost block at any coordinate
-#     # There are four types available in a world slice:
-#     # - 'WORLD_SURFACE': The top non-air blocks
-#     # - 'MOTION_BLOCKING': The top blocks with a hitbox or fluid
-#     # - 'MOTION_BLOCKING_NO_LEAVES': Like MOTION_BLOCKING but ignoring leaves
-#     # - 'OCEAN_FLOOR': The top solid blocks
-#     heights = WORLDSLICE.heightmaps["MOTION_BLOCKING_NO_LEAVES"]
-
-#     print("Building east-west walls...")
-
-#     for x in range(STARTX, LASTX + 1):
-#         # The northern wall
-#         y = heights[(x - STARTX, 0)]
-#         geo.placeCuboid(ED, (x, y - 2, STARTZ), (x, y, STARTZ), Block("granite"))
-#         geo.placeCuboid(ED, (x, y + 1, STARTZ), (x, y + 4, STARTZ), Block("granite_wall"))
-#         # The southern wall
-#         y = heights[(x - STARTX, LASTZ - STARTZ)]
-#         geo.placeCuboid(ED, (x, y - 2, LASTZ), (x, y, LASTZ), Block("red_sandstone"))
-#         geo.placeCuboid(ED, (x, y + 1, LASTZ), (x, y + 4, LASTZ), Block("red_sandstone_wall"))
-
-#     print("Building north-south walls...")
-
-#     for z in range(STARTZ, LASTZ + 1):
-#         # The western wall
-#         y = heights[(0, z - STARTZ)]
-#         geo.placeCuboid(ED, (STARTX, y - 2, z), (STARTX, y, z), Block("sandstone"))
-#         geo.placeCuboid(ED, (STARTX, y + 1, z), (STARTX, y + 4, z), Block("sandstone_wall"))
-#         # The eastern wall
-#         y = heights[(LASTX - STARTX, z - STARTZ)]
-#         geo.placeCuboid(ED, (LASTX, y - 2, z), (LASTX, y, z), Block("prismarine"))
-#         geo.placeCuboid(ED, (LASTX, y + 1, z), (LASTX, y + 4, z), Block("prismarine_wall"))
-
-
-# def buildRoads():
-#     """Build a road from north to south and east to west."""
-#     xaxis = STARTX + (LASTX - STARTX) // 2  # Getting start + half the length
-#     zaxis = STARTZ + (LASTZ - STARTZ) // 2
-#     heights = WORLDSLICE.heightmaps["MOTION_BLOCKING_NO_LEAVES"]
-
-#     print("Calculating road height...")
-#     # Caclulating the average height along where we want to build our road
-#     y = heights[(xaxis - STARTX, zaxis - STARTZ)]
-#     for x in range(STARTX, LASTX + 1):
-#         newy = heights[(x - STARTX, zaxis - STARTZ)]
-#         y = (y + newy) // 2
-#     for z in range(STARTZ, LASTZ + 1):
-#         newy = heights[(xaxis - STARTX, z - STARTZ)]
-#         y = (y + newy) // 2
-
-#     # GLOBAL
-#     # By calling 'global ROADHEIGHT' we allow writing to ROADHEIGHT.
-#     # If 'global' is not called, a new, local variable is created.
-#     global ROADHEIGHT
-#     ROADHEIGHT = y
-
-#     print("Building east-west road...")
-
-#     geo.placeCuboid(ED, (xaxis - 2, y, STARTZ), (xaxis - 2, y, LASTZ), Block("end_stone_bricks"))
-#     geo.placeCuboid(ED, (xaxis - 1, y, STARTZ), (xaxis + 1, y, LASTZ), Block("gold_block"))
-#     geo.placeCuboid(ED, (xaxis + 2, y, STARTZ), (xaxis + 2, y, LASTZ), Block("end_stone_bricks"))
-#     geo.placeCuboid(ED, (xaxis - 1, y + 1, STARTZ), (xaxis + 1, y + 3, LASTZ), Block("air"))
-
-#     print("Building north-south road...")
-
-#     geo.placeCuboid(ED, (STARTX, y, zaxis - 2), (LASTX, y, zaxis - 2), Block("end_stone_bricks"))
-#     geo.placeCuboid(ED, (STARTX, y, zaxis - 1), (LASTX, y, zaxis + 1), Block("gold_block"))
-#     geo.placeCuboid(ED, (STARTX, y, zaxis + 2), (LASTX, y, zaxis + 2), Block("end_stone_bricks"))
-#     geo.placeCuboid(ED, (STARTX, y + 1, zaxis - 1), (LASTX, y + 3, zaxis + 1), Block("air"))
-
-
-# def buildCity():
-#     xaxis = STARTX + (LASTX - STARTX) // 2  # Getting center
-#     zaxis = STARTZ + (LASTZ - STARTZ) // 2
-#     y = ROADHEIGHT
-
-#     print("Building city platform...")
-#     # Building a platform and clearing a dome for the city to sit in
-#     geo.placeCylinder(ED, (xaxis, y,      zaxis), 39, 1, Block("end_stone_bricks"))
-#     geo.placeCylinder(ED, (xaxis, y,      zaxis), 37, 1, Block("gold_block"))
-#     geo.placeCylinder(ED, (xaxis, y +  1, zaxis), 37, 3, Block("air"))
-#     geo.placeCylinder(ED, (xaxis, y +  4, zaxis), 35, 2, Block("air"))
-#     geo.placeCylinder(ED, (xaxis, y +  6, zaxis), 33, 1, Block("air"))
-#     geo.placeCylinder(ED, (xaxis, y +  7, zaxis), 32, 1, Block("air"))
-#     geo.placeCylinder(ED, (xaxis, y +  8, zaxis), 27, 1, Block("air"))
-#     geo.placeCylinder(ED, (xaxis, y +  9, zaxis), 21, 1, Block("air"))
-#     geo.placeCylinder(ED, (xaxis, y + 10, zaxis), 13, 1, Block("air"))
-#     geo.placeCylinder(ED, (xaxis, y + 11, zaxis),  3, 1, Block("air"))
-
-#     for _ in range(50):
-#         buildTower(randint(xaxis - 20, xaxis + 20),
-#                    randint(zaxis - 20, zaxis + 20))
-
-#     # Place a book on a Lectern.
-#     # See the wiki for book formatting codes.
-#     ED.placeBlock((xaxis, y, zaxis), Block("emerald_block"))
-#     bookData = mt.bookData("This book has a page!")
-#     et.placeLectern(ED, (xaxis, y + 1, zaxis), bookData=bookData)
-
-
-# def buildTower(x, z):
-#     radius = 3
-#     diameter = 2*radius + 1
-#     y = ROADHEIGHT
-
-#     print(f"Building tower at {x}, {z}...")
-#     # If the blocks to the north, south, east and west aren't all gold
-#     if not (
-#             ED.getBlock((x - radius, y, z)).id == "minecraft:gold_block"
-#         and ED.getBlock((x + radius, y, z)).id == "minecraft:gold_block"
-#         and ED.getBlock((x, y, z - radius)).id == "minecraft:gold_block"
-#         and ED.getBlock((x, y, z + radius)).id == "minecraft:gold_block"
-#     ):
-#         return  # Return without building anything
-
-#     # Lay the foundation
-#     geo.placeCylinder(ED, (x, y, z), diameter, 1, Block("emerald_block"))
-
-#     # Build ground floor
-#     geo.placeCylinder(ED, (x, y + 1, z), diameter, 3, Block("lime_concrete"), tube=True)
-
-#     # Extend height
-#     height = randint(5, 20)
-#     geo.placeCylinder(ED, (x, y + 4, z), diameter, height, Block("lime_concrete"), tube=True)
-#     height += 4
-
-#     # Build roof
-#     geo.placeCylinder(ED, (x, y + height, z), diameter, 1, Block("emerald_block"))
-#     geo.placeCylinder(ED, (x, y + height + 1, z), diameter-2, 2, Block("emerald_block"))
-#     geo.placeCuboid(ED, (x, y + height, z), (x, y + height + 2, z), Block("lime_stained_glass"))
-#     ED.placeBlock((x, y + 1, z), Block("beacon"))
-
-#     # Trim sides and add windows and doors
-#     # NOTE: When placing doors, you only need to place the bottom block.
-#     geo.placeCuboid(ED, (x + radius, y + 1, z), (x + radius, y + height + 2, z), Block("air"))
-#     geo.placeCuboid(ED, (x + radius - 1, y + 1, z), (x + radius - 1, y + height + 2, z), Block("lime_stained_glass"))
-#     ED.placeBlock((x + radius - 1, y + 1, z), Block("warped_door", {"facing": "west"}))
-
-#     geo.placeCuboid(ED, (x - radius, y + 1, z), (x - radius, y + height + 2, z), Block("air"))
-#     geo.placeCuboid(ED, (x - radius + 1, y + 1, z), (x - radius + 1, y + height + 2, z), Block("lime_stained_glass"))
-#     ED.placeBlock((x - radius + 1, y + 1, z), Block("warped_door", {"facing": "east"}))
-
-#     geo.placeCuboid(ED, (x, y + 1, z + radius), (x, y + height + 2, z + radius), Block("air"))
-#     geo.placeCuboid(ED, (x, y + 1, z + radius - 1), (x, y + height + 2, z + radius - 1), Block("lime_stained_glass"))
-#     ED.placeBlock((x, y + 1, z + radius - 1), Block("warped_door", {"facing": "north"}))
-
-#     geo.placeCuboid(ED, (x, y + 1, z - radius), (x, y + height + 2, z - radius), Block("air"))
-#     geo.placeCuboid(ED, (x, y + 1, z - radius + 1), (x, y + height + 2, z - radius + 1), Block("lime_stained_glass"))
-#     ED.placeBlock((x, y + 1, z - radius + 1), Block("warped_door", {"facing": "south"}))
-
-
-# def main():
-#     try:
-#         buildPerimeter()
-#         buildRoads()
-#         buildCity()
-
-#         print("Done!")
-
-#     except KeyboardInterrupt: # useful for aborting a run-away program
-#         print("Pressed Ctrl-C to kill program.")
-
-
-# # === STRUCTURE #4
-# # The code in here will only run if we run the file directly (not imported).
-# # This prevents people from accidentally running your generator.
-# # It is recommended to directly call a function here, because any variables
-# # you declare outside a function will be global.
-# if __name__ == '__main__':
-#     main()
